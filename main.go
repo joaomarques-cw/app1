3ab33c25b6d6e6a064f16b4c2888b9cf7608f82c
joaomarques-cw <joao.marques@cloudwalk.io>
2024-10-02 10:32:56 -0300

    includes adjusts

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 		fmt.Fprintf(w, "Olá! Esta é uma aplicação simples para implantação no Kubernetes/ArgoCD.")
 	})
 
-	porta := ":8080"
+	porta := ":9090"
 	fmt.Printf("Servidor iniciado na porta %s\n", porta)
 	if err := http.ListenAndServe(porta, nil); err != nil {
 		fmt.Printf("Erro ao iniciar o servidor: %v\n", err)
